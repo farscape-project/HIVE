a61167c50ec3e87592939c50fe2553a54d74186e
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2024-02-22 14:05:36 +0000

    Create MOOSE app for HIVE

---
+++ src/base/hiveApp.C
@@ -0,0 +1,51 @@
+#include "hiveApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+hiveApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+  params.set<bool>("use_legacy_material_output") = false;
+  params.set<bool>("use_legacy_initial_residual_evaluation_behavior") = false;
+  return params;
+}
+
+hiveApp::hiveApp(InputParameters parameters) : MooseApp(parameters)
+{
+  hiveApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+hiveApp::~hiveApp() {}
+
+void
+hiveApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ModulesApp::registerAllObjects<hiveApp>(f, af, s);
+  Registry::registerObjectsTo(f, {"hiveApp"});
+  Registry::registerActionsTo(af, {"hiveApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+hiveApp::registerApps()
+{
+  registerApp(hiveApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+hiveApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  hiveApp::registerAll(f, af, s);
+}
+extern "C" void
+hiveApp__registerApps()
+{
+  hiveApp::registerApps();
+}
